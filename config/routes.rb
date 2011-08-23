f0532f70125c989cf77e3d110d23602db5dd9a99
slim <slimbl@gmail.com>
2011-08-24 00:01:26 +0100

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   root :to => 'pages#home'
-  
+  match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
     match '/about',   :to => 'pages#about'
     match '/help',    :to => 'pages#help'
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,5 +21,19 @@ describe "LayoutLinks" do
         get '/help'
         response.should have_selector('title', :content => "Help")
       end
- # end
+      
+      it "should have a signup page at '/signup'" do
+          get '/signup'
+          response.should have_selector('title', :content => "Sign up")
+        end
+        
+       it "should have the right links on the layout"  do
+          visit root_path 
+          response.should have_selector('title', :content => "Home")
+         click_link "About"
+         response.should have_selector('title', :content => "About")
+          
+        end
+        
+         # end
 end
