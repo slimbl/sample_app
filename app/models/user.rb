17722cf275d73a3b8498aa08fc07ddcf59e53e86
slim <slimbl@gmail.com>
2011-09-18 21:16:32 +0100

    Finished the password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#
+
+
+
 class User < ActiveRecord::Base 
   
   attr_accessor :password #, :password_confirmation
@@ -15,18 +29,41 @@ class User < ActiveRecord::Base
     validates :password, :presence     => true,
                         :confirmation => true,
                         :length       => { :within => 6..40 }
+                        
+   before_save :encrypt_password # le callback
+   
+   
+   def has_password?(submitted_password)
+       encrypted_password == encrypt(submitted_password)
+     end
+
+     def self.authenticate(email, submitted_password)
+         user = find_by_email(email)
+         return nil  if user.nil?
+         return user if user.has_password?(submitted_password)
+       end
+  
+
+
+     private
+
+       def encrypt_password
+         self.salt = make_salt unless has_password?(password)
+         self.encrypted_password = encrypt(password)
+       end
+
+       def encrypt(string)
+         secure_hash("#{salt}--#{string}")
+       end
+
+       def make_salt
+         secure_hash("#{Time.now.utc}--#{password}")
+       end
+
+       def secure_hash(string)
+         Digest::SHA2.hexdigest(string)
+       end
+   
     
 end
  
-# == Schema Information
-#
-# Table name: users
-#
-#  id                 :integer         not null, primary key
-#  name               :string(255)
-#  email              :string(255)
-#  created_at         :datetime
-#  updated_at         :datetime
-#  encrypted_password :string(255)
-#
-
